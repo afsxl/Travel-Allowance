b63cb11f323acb03ff3236b7c8b6999a2c2c387b
Lolan <muhd.afsxl@gmail.com>
2025-02-02 16:19:41 +0530

    Added duplication

--- ta/views.py
+++ ta/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import *
-from django.http import JsonResponse
 
 
 def login_view(request):
